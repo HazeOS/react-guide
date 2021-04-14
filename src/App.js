3838c5f84c5ca7c6a0f64a013c5bf7c2777b05ad
HazeOS <riper752@gmail.com>
2021-04-14 15:22:34 +0300

    396. Props & State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>This is React Application.</h1>
+    state = {
+        persons: [
+            {name: 'Ilya', age: 23},
+            {name: 'Vasyl', age: 25},
+            {name: 'Peta', age: 30}
+        ]
+    }
 
-          <Person name="Ilya" age="23">My hobby: Nothing</Person>
-          <Person name="Vasyl" age="25"/>
-          <Person name="Peta" age="30"/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>This is React Application.</h1>
+
+                <p><b>Using State</b></p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby: Nothing</Person>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+                <br/>
+
+                <p><b>Without using State</b></p>
+                <Person name="Ilya" age="23">My hobby: Nothing</Person>
+                <Person name="Vasyl" age="25"/>
+                <Person name="Peta" age="30"/>
+            </div>
+        );
+    }
 }
 
 export default App;
