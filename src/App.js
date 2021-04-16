95ffffe0204758957c039d90a200718208a0d5a6
HazeOS <riper752@gmail.com>
2021-04-16 11:11:45 +0300

    406. Styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,79 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    state = {
+const app = (props) => {
+    const [personsState, setPersonsState] = useState({
         persons: [
             {name: 'Ilya', age: 23},
             {name: 'Vasyl', age: 25},
             {name: 'Peta', age: 30}
         ]
+    });
+
+    const [otherState, setOtherState] = useState('someValue');
+
+    console.log(personsState, otherState);
+
+    const switchNameHandler = (newName) => {
+        setPersonsState({
+            persons: [
+                {name: newName, age: 18},
+                {name: 'Vasyl', age: 25},
+                {name: 'Peta', age: 30}
+            ]
+        });
     }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>This is React Application.</h1>
-
-                <p><b>Using State</b></p>
-                <button>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby: Nothing</Person>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-
-                <br/>
-
-                <p><b>Without using State</b></p>
-                <Person name="Ilya" age="23">My hobby: Nothing</Person>
-                <Person name="Vasyl" age="25"/>
-                <Person name="Peta" age="30"/>
-            </div>
-        );
+    const nameChangedHandler = (event) => {
+        setPersonsState({
+            persons: [
+                {name: 'Ilya', age: 18},
+                {name: event.target.value, age: 25},
+                {name: 'Peta', age: 30}
+            ]
+        });
     }
+
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    }
+
+    return (
+        <div className="App">
+            <h1>This is React Application.</h1>
+
+            <p><b>Using State</b></p>
+            <button
+                style={style}
+                onClick={() => switchNameHandler('ILYAS from arrow function')}>
+                Switch Name
+            </button>
+            <Person
+                name={personsState.persons[0].name}
+                age={personsState.persons[0].age}>
+                My hobby: Nothing
+            </Person>
+
+            <Person
+                name={personsState.persons[1].name}
+                age={personsState.persons[1].age}
+                click={switchNameHandler.bind(this, 'ILYAS from BIND')}
+                changed={nameChangedHandler}
+            />
+
+            <Person
+                name={personsState.persons[2].name}
+                age={personsState.persons[2].age}
+                click={() => switchNameHandler('ILYAS from arrow function')}
+            />
+
+        </div>
+    );
 }
 
-export default App;
+export default app;
