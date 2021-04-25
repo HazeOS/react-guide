b63ad3469d371970d9ab9acd043840db8e47e001
HazeOS <riper752@gmail.com>
2021-04-25 15:47:20 +0300

    - 444. Splitting app into components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import classes from './Person.module.css';
+import React from "react";
+import classes from "./Person.module.css";
 
 const person = (props) => {
     if (props.name.length > 15) {
@@ -8,7 +8,7 @@ const person = (props) => {
 
     return (
         <div className={classes.Person}>
-            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old.</p>
+            <p onClick={props.clicked}>I'm a {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} value={props.name}/>
         </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import classes from './App.module.css';
-import Person from '../components/Persons/Person/Person';
-import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
     state = {
@@ -56,48 +56,25 @@ class App extends Component {
 
     render() {
         let persons = null;
-        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
-                <div>
-                    {this.state.persons.map((person) => {
-                        return (
-                            <ErrorBoundary key={person.id}>
-                                <Person
-                                    name={person.name}
-                                    age={person.age}
-                                    changed={event => this.nameChangedHandler(event, person.id)}
-                                    click={() => this.deletePersonHandler(person.id)}>
-                                    My hobby: Nothing
-                                </Person>
-                            </ErrorBoundary>
-                        );
-                    })}
-                </div>
+                <Persons
+                    persons={this.state.persons}
+                    clicked={this.deletePersonHandler}
+                    changed={this.nameChangedHandler}
+                />
             );
-
-            btnClass = classes.Red;
-        }
-
-        const assignedClasses = [];
-        if (this.state.persons.length <= 2) {
-            assignedClasses.push(classes.red); // classes = ['red']
-        }
-        if (this.state.persons.length <= 1) {
-            assignedClasses.push(classes.bold); // classes = ['red', 'bold']
         }
 
         return (
             <div className={classes.App}>
-                <h1>This is React Application.</h1>
-                <p className={assignedClasses.join(' ')}>And It's working</p>
+                <Cockpit
+                    showPersons={this.state.showPersons}
+                    persons={this.state.persons}
+                    clicked={this.showPersonsHandler}
+                />
 
-                <button
-                    className={btnClass}
-                    onClick={this.showPersonsHandler}>
-                    Show persons
-                </button>
                 {persons}
             </div>
         );
